8d12fb0e6c782776500e97219f8b2e0d2d9db17c
bintina425 <bintina425@gmail.com>
2025-03-16 16:19:22 +0000

    Add 10.go

---
+++ src/10.go
@@ -0,0 +1,13 @@
+  package main
+  
+import "fmt"
+  
+func main() {
+	result := solveProblem(1234)
+	fmt.Println(result)
+}
+  
+func solveProblem(n int) int {
+	// Your solution here
+	return n * 2
+}
