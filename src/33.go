bac183bbc2b29128c23cbec6cc39a44a9a02afae
bintina425 <bintina425@gmail.com>
2025-04-25 18:24:42 +0000

    Add 33.go

---
+++ src/33.go
@@ -0,0 +1,9 @@
+public void main() {
+    // Define variables
+    double x = 0.5;
+    double y = 1.2;
+
+    // Perform calculations and display results
+    System.out.println("The square root of " + x + " is approximately " + Math.sqrt(x));
+    System.out.println("The cube root of " + y + " is approximately " + Math.cbrt(y));
+}
